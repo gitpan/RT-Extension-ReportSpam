80abf4cbf9c9e7ca2f7b2634f65900160329ee8d
Thomas Sibley <tsibley@cpan.org>
2013-04-02 16:34:20 +0000

    Import of TSIBLEY/RT-Extension-ReportSpam-0.10 from CPAN.
    gitpan-cpan-distribution: RT-Extension-ReportSpam
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         TSIBLEY/RT-Extension-ReportSpam-0.10.tar.gz
    gitpan-cpan-author:       TSIBLEY
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/ReportSpam.pm
+++ lib/RT/Extension/ReportSpam.pm
@@ -4,21 +4,26 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.10';
 
 =head1 NAME
 
-RT::Extension::ReportSpam - report a spam in one click in RT
+RT::Extension::ReportSpam - mark tickets as spam with one click
 
 =head1 DESCRIPTION
 
-A simple extension that works with RT 3.6 and newer and allows users
+A simple extension that works with RT 4.0 and newer and allows users
 to report a ticket as a spam message.
 
 To find all tickets reported as spam you have to use query builder in
 advanced mode with the following query:
 
     HasAttribute = 'SpamReports'
+
+=head1 RT 3.8
+
+Users of this extension on RT 3.8 should not upgrade past version 0.03 of this
+extension.
 
 =cut
 
